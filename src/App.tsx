89411d192a2415e1cbd692e5e4c4c807d1657414
pdevulapally <128475072+pdevulapally@users.noreply.github.com>
2025-06-27 17:31:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -17,27 +17,69 @@ import ZoomCallback from "./pages/ZoomCallback";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <LanguageProvider>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/booking" element={<BookingPage />} />
-            <Route path="/service/:serviceId" element={<ServiceDetailPage />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route path="/auth/zoom/callback" element={<ZoomCallback />} />
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route path="dashboard" element={<AdminDashboard />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </LanguageProvider>
+  <HelmetProvider>
+    <LanguageProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              {/* Homepage Routes */}
+              <Route path="/" element={<Index />} />
+              <Route path="/te" element={<Index />} />
+              
+              {/* Booking Routes */}
+              <Route path="/booking" element={<BookingPage />} />
+              <Route path="/te/booking" element={<BookingPage />} />
+              <Route path="/book-puja" element={<BookingPage />} />
+              <Route path="/te/book-puja" element={<BookingPage />} />
+              
+              {/* Service Detail Routes */}
+              <Route path="/service/:serviceId" element={<ServiceDetailPage />} />
+              <Route path="/te/service/:serviceId" element={<ServiceDetailPage />} />
+              
+              {/* Individual Service Routes for SEO */}
+              <Route path="/satyanarayana-vratam" element={<ServiceDetailPage />} />
+              <Route path="/te/satyanarayana-vratam" element={<ServiceDetailPage />} />
+              <Route path="/gruhapravesam" element={<ServiceDetailPage />} />
+              <Route path="/te/gruhapravesam" element={<ServiceDetailPage />} />
+              <Route path="/hindu-wedding" element={<ServiceDetailPage />} />
+              <Route path="/te/hindu-wedding" element={<ServiceDetailPage />} />
+              <Route path="/barasala" element={<ServiceDetailPage />} />
+              <Route path="/te/barasala" element={<ServiceDetailPage />} />
+              <Route path="/upanayanam" element={<ServiceDetailPage />} />
+              <Route path="/te/upanayanam" element={<ServiceDetailPage />} />
+              <Route path="/seemantham" element={<ServiceDetailPage />} />
+              <Route path="/te/seemantham" element={<ServiceDetailPage />} />
+              <Route path="/annaprasana" element={<ServiceDetailPage />} />
+              <Route path="/te/annaprasana" element={<ServiceDetailPage />} />
+              <Route path="/pitra-paksha" element={<ServiceDetailPage />} />
+              <Route path="/te/pitra-paksha" element={<ServiceDetailPage />} />
+              <Route path="/ganesha-puja" element={<ServiceDetailPage />} />
+              <Route path="/te/ganesha-puja" element={<ServiceDetailPage />} />
+              <Route path="/lakshmi-puja" element={<ServiceDetailPage />} />
+              <Route path="/te/lakshmi-puja" element={<ServiceDetailPage />} />
+              <Route path="/durga-puja" element={<ServiceDetailPage />} />
+              <Route path="/te/durga-puja" element={<ServiceDetailPage />} />
+              <Route path="/online-puja" element={<ServiceDetailPage />} />
+              <Route path="/te/online-puja" element={<ServiceDetailPage />} />
+              
+              {/* Admin Routes */}
+              <Route path="/admin/login" element={<AdminLogin />} />
+              <Route path="/auth/zoom/callback" element={<ZoomCallback />} />
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route path="dashboard" element={<AdminDashboard />} />
+              </Route>
+              
+              {/* 404 Route */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </LanguageProvider>
+  </HelmetProvider>
 );
 
 export default App;
